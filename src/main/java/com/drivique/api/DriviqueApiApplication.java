9098b63b265d519a349231fe7c20b2e5a15dd6f1
danna barrios <dannabarrios0820@gmail.com>
2026-09-02 22:19:55 -0500

    chore: bootstrap Spring Boot backend structure

---
+++ src/main/java/com/drivique/api/DriviqueApiApplication.java
@@ -0,0 +1,13 @@
+package com.drivique.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriviqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriviqueApiApplication.class, args);
+	}
+
+}
